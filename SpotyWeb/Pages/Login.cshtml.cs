d61156996758affbebd792687bfae1fe416d0bf5
jonathan <jhoni65475@gmail.com>
2021-04-14 23:00:20 -0500

    Empezando a darle funcionalidad al login

--- SpotyWeb/Pages/Login.cshtml.cs
+++ SpotyWeb/Pages/Login.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpotyWeb.ModelosBD;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SpotyWeb.Pages
@@ -11,6 +12,29 @@ namespace SpotyWeb.Pages
     {
         public void OnGet()
         {
+            
+               /* using (SpotyWebBDContext db = new SpotyWebBDContext())
+                {
+                    var usu = db.Usuario.Where((x) => x.Nickname == usuario.Nickname).FirstOrDefault();
+                    if (usu != null)
+                    {
+                        if (usu.Password.Equals(usuario.Password))
+                        {
+
+                            Callback.GetLoginResult(LoginResults.UsuarioEncontrado);
+                        }
+                        else
+                        {
+                            Callback.GetLoginResult(LoginResults.ContraseñaIncorrecta);
+                        }
+                    }
+                    else
+                    {
+                        Callback.GetLoginResult(LoginResults.NoExisteUrsuario);
+                    }
+                }
+
+            */
         }
     }
 }
