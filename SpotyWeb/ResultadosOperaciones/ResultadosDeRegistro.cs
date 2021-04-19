88fd895922990ab7e19289f794bb07968d410112
jonathan <jhoni65475@gmail.com>
2021-04-19 12:51:43 -0500

    Agregando la funcionalidad de registro

---
+++ SpotyWeb/ResultadosOperaciones/ResultadosDeRegistro.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotyWeb.ResultadosOperaciones
+{
+    public class Resultados_de_registro
+    {
+    }
+}
